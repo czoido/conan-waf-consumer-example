17c232cb59e6cfe441ddb9d0791ccb091444e5eb
czoido <mrgalleta@gmail.com>
2019-06-20 12:32:30 +0200

    first commit

---
+++ conanfile.py
@@ -0,0 +1,25 @@
+import os
+from shutil import copy
+
+from conans import ConanFile, tools, python_requires
+
+base = python_requires("waf-build-helper/0.1@czoido/testing")
+
+
+class TestWafConan(base.get_conanfile()):
+    settings = "os", "compiler", "build_type", "arch", "arch_build"
+    generators = "Waf"
+    requires = "WafGen/0.1@czoido/testing", "mylib-waf/1.0@czoido/testing"
+    build_requires = "waf/2.0.17@czoido/testing"
+    exports = "wscript", "main.cpp"
+
+    def build(self):
+        waf = base.WafBuildEnvironment(self)
+        waf.configure()
+        waf.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            self._bin_path = "build"
+            self.run(os.path.join(self.source_folder, self._bin_path,
+                                  "test_package"), run_environment=True)
